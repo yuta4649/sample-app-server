472ac0fb4b2c7c25db1753c66c0b288c2d890c07
Yuta Mori <yuta46de49@gmail.com>
2022-07-24 22:27:51 +0900

    add: admin root user

--- src/main/resources/db/migration/USER_DATA.sql
+++ src/main/resources/db/migration/USER_DATA.sql
@@ -1,1 +1,3 @@
-INSERT INTO USERS values ('1000', N'森 雄太', 'yuta46de49@gmail.com', 'YutaMori', 'Yuta5963', null, 'ROLE_ADMIN', null, 0, '0', '0', 'INIT', '2021-09-22','INIT', '2021-09-22', 0);
+INSERT INTO USERS values ('1000', 'root', null, 'root', 'P@ssword_000', null, 'ROLE_ROOT', null, 0, '0', '0', 'INIT', '2021-09-22','INIT', '2021-09-22', 0);
+INSERT INTO USERS values ('1001', 'admin', null, 'admin', 'P@ssword_000', null, 'ROLE_ADMIN', null, 0, '0', '0', 'INIT', '2021-09-22','INIT', '2021-09-22', 0);
+INSERT INTO USERS values ('1002', N'森 雄太', 'yuta46de49@gmail.com', 'YutaMori', 'Yuta5963', null, 'ROLE_ADMIN', null, 0, '0', '0', 'INIT', '2021-09-22','INIT', '2021-09-22', 0);
