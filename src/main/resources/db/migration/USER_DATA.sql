06630a1ea6ba510ca65f6f528117c080b1ca5898
Yuta Mori <yuta46de49@gmail.com>
2022-07-31 21:15:00 +0900

    fix user_data

--- src/main/resources/db/migration/USER_DATA.sql
+++ src/main/resources/db/migration/USER_DATA.sql
@@ -1,3 +1,4 @@
 INSERT INTO USERS values ('1000', 'root', null, 'root', 'P@ssword_000', null, 'ROLE_ROOT', null, 0, '0', '0', 'INIT', '2021-09-22','INIT', '2021-09-22', 0);
 INSERT INTO USERS values ('1001', 'admin', null, 'admin', 'P@ssword_000', null, 'ROLE_ADMIN', null, 0, '0', '0', 'INIT', '2021-09-22','INIT', '2021-09-22', 0);
 INSERT INTO USERS values ('1002', N'森 雄太', 'yuta46de49@gmail.com', 'YutaMori', 'Yuta5963', null, 'ROLE_ADMIN', null, 0, '0', '0', 'INIT', '2021-09-22','INIT', '2021-09-22', 0);
+INSERT INTO USERS values ('1003', 'test', null, 'test', 'P@ssword_000', null, 'ROLE_LOCAL', null, 0, '0', '0', 'INIT', '2021-09-22','INIT', '2021-09-22', 0);
